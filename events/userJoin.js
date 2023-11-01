9c2901be04cc33d6a37060719c886e3da470bbc5
Vabolos <79908586+Vabolos@users.noreply.github.com>
2023-11-01 10:52:39 +0100

    updated userJoin.js to not add the same user twice (or more) to the database

--- events/userJoin.js
+++ events/userJoin.js
@@ -4,17 +4,34 @@ const dbConnection = require('../dbConnection.js');
 module.exports = {
 	name: Events.GuildMemberAdd,
 	async execute(member) {
+		if (member.user.bot) return;
 
-		// get info from the joined user
+		// Get info from the joined user
 		const userName = member.user.username;
 		const userId = member.user.id;
 
-		const sql = 'INSERT INTO `joins`(`userName`, `userId`) VALUES (?, ?)';
-		const values = [userName, userId];
+		const checkSql = 'SELECT COUNT(*) AS userCount FROM `joins` WHERE `userId` = ?';
+		const checkValues = [userId];
 
-		dbConnection.query(sql, values, (err) => {
-			if (err) {
-				console.error('Error:', err);
+		// Check if the user is already in the database
+		dbConnection.query(checkSql, checkValues, (checkErr, results) => {
+			if (checkErr) {
+				console.error('Error checking database:', checkErr);
+				return;
+			}
+
+			const userCount = results[0].userCount;
+
+			if (userCount === 0) {
+				// User is not in the database, so insert the new record
+				const insertSql = 'INSERT INTO `joins` (`userName`, `userId`) VALUES (?, ?)';
+				const insertValues = [userName, userId];
+
+				dbConnection.query(insertSql, insertValues, (insertErr) => {
+					if (insertErr) {
+						console.error('Error inserting into database:', insertErr);
+					}
+				});
 			}
 		});
 	},
