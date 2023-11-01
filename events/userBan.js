c8b8b7d7468bcad13277ca6b0237b82b78c290ad
Vabolos <79908586+Vabolos@users.noreply.github.com>
2023-11-01 13:54:28 +0100

    banned users will now be stored in the database. confirmation in console logs

---
+++ events/userBan.js
@@ -0,0 +1,29 @@
+const { Events, AuditLogEvent } = require('discord.js');
+const dbConnection = require('../dbConnection.js');
+
+module.exports = {
+    name: Events.GuildAuditLogEntryCreate,
+    once: false,
+    async execute(entry) {
+        if (entry.action === AuditLogEvent.MemberBanAdd) {
+            const { target, reason, executor } = entry;
+
+            const userName = target.username;
+            const userId = target.id;
+            const executorName = executor.username;
+            const executorId = executor.id;
+
+            const sql = 'INSERT INTO bans (userName, userId, reason, executorName, executorId) VALUES (?, ?, ?, ?, ?)';
+            const values = [userName, userId, reason, executorName, executorId];
+
+            dbConnection.query(sql, values, (err) => {
+                if (err) {
+                    console.error('Error:', err);
+                }
+                else {
+                    console.log('User banned and data inserted into the database.');
+                }
+            });
+        }
+    },
+};
