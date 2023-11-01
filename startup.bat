3f20d68559716c43609c943d27c70f351284eaf2
Vabolos <vaboloslol@gmail.com>
2023-11-02 00:16:26 +0100

    added a batch file to make starting the server easier (along with auto start for bot)

---
+++ startup.bat
@@ -0,0 +1,18 @@
+@echo off
+title Discord Bot and XAMPP Launcher
+
+@REM Start XAMPP
+start "XAMPP Control Panel" "C:\xampp\xampp-control.exe" 
+
+@REM Wait for XAMPP services to start (adjust the sleep time as needed)
+timeout /t 10
+
+
+@REM Change the directory to where your Discord bot script is located
+cd "D:\Github\Bots\Discord.js\discord.js-dashboard"
+
+REM Start your Discord bot using Node.js
+nodemon
+
+@REM Keep the batch file open so you can see any console output
+pause
