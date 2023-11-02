fe547ec8c53a091d25d823305bfda040ff66f482
Vabolos <vaboloslol@gmail.com>
2023-11-02 12:11:37 +0100

    added a better console message for when the bot is ready

--- bot/events/clientReady.js
+++ bot/events/clientReady.js
@@ -5,6 +5,7 @@ module.exports = {
 	once: true,
 	async execute(client) {
 		console.log(`Ready!\nLogged in as ${client.user.tag}`);
+		console.log('\x1b[36m|\n└── Use CTRL+C to exit.');
 		client.user.setPresence({ activities: [{ name: 'Javascript' }] });
 	},
 };
