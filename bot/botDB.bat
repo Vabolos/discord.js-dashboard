07b7ab7f17b033b615db75138677b32b02eb2af4
Vabolos <vaboloslol@gmail.com>
2023-11-02 12:12:06 +0100

    new batfile structure, now opens a tab for each bat file instead of a new window

---
+++ bot/botDB.bat
@@ -0,0 +1,21 @@
+@echo off
+title Discord Bot and XAMPP Launcher
+
+@REM Start XAMPP
+start "XAMPP Control Panel" "C:\xampp\xampp-control.exe" 
+
+@REM automatically start Apache and MySQL
+net start apache
+net start mysql
+
+@REM Wait for XAMPP services to start (adjust the sleep time as needed)
+timeout /t 10
+
+@REM Change the directory to where your Discord bot script is located
+cd "D:\Github\Bots\Discord.js\discord.js-dashboard\bot"
+
+REM Start your Discord bot using nodemon (or node . if you don't use nodemon/have it installed)
+nodemon
+
+@REM Keep the batch file open so you can see any console output
+pause
--- startup.bat
+++ startup.bat
@@ -1,18 +1,1 @@
-@echo off
-title Discord Bot and XAMPP Launcher
-
-@REM Start XAMPP
-start "XAMPP Control Panel" "C:\xampp\xampp-control.exe" 
-
-@REM Wait for XAMPP services to start (adjust the sleep time as needed)
-timeout /t 10
-
-
-@REM Change the directory to where your Discord bot script is located
-cd "D:\Github\Bots\Discord.js\discord.js-dashboard\bot"
-
-REM Start your Discord bot using nodemon (or node . if you don't use nodemon/have it installed)
-nodemon
-
-@REM Keep the batch file open so you can see any console output
-pause
+start /b "" wt ^ D:\Github\Bots\Discord.js\discord.js-dashboard\bot\BotDB.bat; ^ D:\Github\Bots\Discord.js\discord.js-dashboard\ViteServer.bat;
