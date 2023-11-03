68540e81d417e8df4ff8c92bec76b63a636d26fb
Vabolos <vaboloslol@gmail.com>
2023-11-03 12:37:44 +0100

    fixed error messages for timeout event

--- bot/events/userTimeout.js
+++ bot/events/userTimeout.js
@@ -4,28 +4,25 @@ const dbConnection = require('../dbConnection.js');
 module.exports = {
     name: Events.GuildAuditLogEntryCreate,
     once: false,
-    async execute(entry) {
-        if (entry.action === AuditLogEvent.MemberUpdate) {
-            const { target, reason, executor } = entry;
+        async execute(entry) {
+            if (entry.action === AuditLogEvent.MemberUpdate && entry.changes[0].key === 'communication_disabled_until') {
+            {
+                const userId = entry.target.id;
+                const userName = entry.target.username;
+                const executor = entry.executor.username;
+                const executorId = entry.executor.id;
+                const timeOut = entry;
+                const reason = timeOut.reason ?? 'No reason provided';
 
-            const userName = target.username;
-            const userId = target.id;
-            const executorName = executor.username;
-            const executorId = executor.id;
+                const insertQuery = 'INSERT INTO timeouts (userName, userId, reason, executorName, executorId) VALUES (?, ?, ?, ?, ?)';
 
-            // Insert the timeout data into the 'timeouts' table
-            const sql = 'INSERT INTO timeouts (userName, userId, reason, executorName, executorId) VALUES (?, ?, ?, ?, ?)';
-            const values = [userName, userId, reason, executorName, executorId];
-
-            // Execute the SQL query
-            dbConnection.query(sql, values, (err) => {
-                if (err) {
-                    console.error('Error:', err);
-                }
-                else {
-                    console.log('\x1b[31m%s\x1b[0m', 'User timed out, and data inserted into the database.');
-                }
-            });
+                dbConnection.query(insertQuery, [userName, userId, reason, executor, executorId], (error) => {
+                    if (error) {
+                        console.error('Error executing insert query:', error);
+                        return;
+                    }
+                });
+            }
         }
     },
 };
