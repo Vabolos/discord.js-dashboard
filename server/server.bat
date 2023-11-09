210f5ccd9fa3a98dfd8610efc7650e104312693e
Vabolos <vaboloslol@gmail.com>
2023-11-09 21:22:50 +0100

    fixed server starte batch file

--- server/server.bat
+++ server/server.bat
@@ -4,6 +4,9 @@ title Fastify Server Starter
 @REM Change the directory to where your server file is located
 cd D:\Github\Bots\Discord.js\discord.js-dashboard\server
 
+@REM wait a few seconds for the server to start
+timeout /t 3 /nobreak
+
 @REM Start your fastify server
 nodemon server
 
