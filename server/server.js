0760d6a24c79e21f14b9aca474fdfd63f629645f
Vabolos <vaboloslol@gmail.com>
2023-11-09 16:46:07 +0100

    queries added to backend server.js (still in progress/need to test)

--- server/server.js
+++ server/server.js
@@ -1,8 +1,60 @@
 const fastify = require('fastify')({ logger: true })
 const dbConnection = require('../dbConnection.js');
 
-fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
+// get total sent messages
+fastify.get('/result', async (request, reply) => {
+  dbConnection.query('SELECT COUNT(message) FROM messageInfo', (error, results) => {
+    if (error) {
+      console.error('Error executing select query:', error);
+      reply.send({ error: 'Error executing select query' });
+      return;
+    }
+    const count = results[0]['COUNT(message)'];
+    const responseData = { count };
+    reply.send(responseData);
+  });
+})
+
+// get total bans
+fastify.get('/result', async (request, reply) => {
+  dbConnection.query('SELECT COUNT(userId) FROM bans', (error, results) => {
+    if (error) {
+      console.error('Error executing select query:', error);
+      reply.send({ error: 'Error executing select query' });
+      return;
+    }
+    const count = results[0]['COUNT(userId)'];
+    const responseData = { count };
+    reply.send(responseData);
+  });
+})
+
+// get total timeouts
+fastify.get('/result', async (request, reply) => {
+  dbConnection.query('SELECT COUNT(userId) FROM timeouts', (error, results) => {
+    if (error) {
+      console.error('Error executing select query:', error);
+      reply.send({ error: 'Error executing select query' });
+      return;
+    }
+    const count = results[0]['COUNT(userId)'];
+    const responseData = { count };
+    reply.send(responseData);
+  });
+})
+
+// get total kicks
+fastify.get('/result', async (request, reply) => {
+  dbConnection.query('SELECT COUNT(userId) FROM kicks', (error, results) => {
+    if (error) {
+      console.error('Error executing select query:', error);
+      reply.send({ error: 'Error executing select query' });
+      return;
+    }
+    const count = results[0]['COUNT(userId)'];
+    const responseData = { count };
+    reply.send(responseData);
+  });
 })
 
 const start = async () => {
