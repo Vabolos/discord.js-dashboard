460af3c5beea87e787febe143217440e28c1dc97
Vabolos <79908586+Vabolos@users.noreply.github.com>
2023-11-30 11:14:34 +0100

    new command to view all current upcomming items

---
+++ bot/commands/itemView.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const dbConnection = require('../dbConnection.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('itemview')
+        .setDescription('View all items in the database'),
+
+    async execute(interaction) {
+        const embed = new EmbedBuilder()
+            .setTitle('Upcoming items')
+            .setDescription('List of all upcoming items')
+            .setColor('#0099ff');
+
+        const sql = 'SELECT `id`, `info` FROM `upcoming`';
+        const values = [];
+
+        dbConnection.query(sql, values, (err, results) => {
+            if (err) {
+                console.error('Error:', err);
+                return interaction.reply('An error occurred while fetching data.');
+            }
+
+            // Loop through all results and add them to the embed
+            results.forEach(result => {
+                embed.addFields(
+                    { name: `ID: ${result.id}`, value: `Info: ${result.info}` },
+                );
+            });
+
+            // Send the populated embed as a reply
+            interaction.reply({ embeds: [embed] });
+        });
+    },
+};
