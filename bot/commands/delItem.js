d0480903b422f44288864aa8fa9032dcb4ea09a4
Vabolos <79908586+Vabolos@users.noreply.github.com>
2023-12-01 14:13:01 +0100

    Fix item deletion and add error handling

--- bot/commands/delItem.js
+++ bot/commands/delItem.js
@@ -5,26 +5,51 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('delitem')
         .setDescription('Remove an item from the dashboard')
-
         .addIntegerOption(option => option.setName('id').setDescription('ID of the item to delete').setRequired(true)),
 
     async execute(interaction) {
         const embed = new EmbedBuilder()
-            .setTitle('Deleted item!')
-            .setDescription(`**Deleted item with ID:** ${interaction.options.getInteger('id')}`)
-            .setColor('#0099ff');
+            .setColor('#342B7E');
 
         const id = interaction.options.getInteger('id');
 
-        const sql = 'DELETE FROM `upcoming` WHERE `id` = ?';
+        // Check if the item ID is 1 (default item), then prevent deletion
+        if (id === 1) {
+            const errorEmbed = new EmbedBuilder()
+                .setTitle('Error!')
+                .setDescription('You cannot delete the default item.')
+                .setColor('#342B7E')
+                .setTimestamp();
+            await interaction.reply({ embeds: [errorEmbed] });
+            return;
+        }
+
+        const sql = 'DELETE FROM `upcoming` WHERE `id` = ? AND `id` <> 1';
         const values = [id];
 
-        dbConnection.query(sql, values, (err) => {
-			if (err) {
-				console.error('Error:', err);
-			}
-		});
-
-        await interaction.reply({ embeds: [embed] });
+        dbConnection.query(sql, values, (err, result) => {
+            if (err) {
+                console.error('Error:', err);
+                const deleteErrorEmbed = new EmbedBuilder()
+                    .setTitle('Error!')
+                    .setDescription('An error occurred while trying to delete the item.')
+                    .setColor('#342B7E')
+                    .setTimestamp();
+                interaction.reply({ embeds: [deleteErrorEmbed] });
+            }
+            else if (result && result.affectedRows > 0) {
+                    embed.setTitle('Deleted item!');
+                    embed.setDescription(`**Deleted item with ID:** ${id}`);
+                    interaction.reply({ embeds: [embed] });
+                }
+                else {
+                    const notFoundEmbed = new EmbedBuilder()
+                        .setTitle('Error!')
+                        .setDescription('Item not found or cannot be deleted.')
+                        .setColor('#342B7E')
+                        .setTimestamp();
+                    interaction.reply({ embeds: [notFoundEmbed] });
+                }
+        });
     },
 };
