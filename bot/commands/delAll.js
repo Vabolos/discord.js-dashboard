c4a0d0ba209a197e71e55ed612f1ff2799e6173a
Vabolos <79908586+Vabolos@users.noreply.github.com>
2023-12-01 14:12:54 +0100

    Refactor deleteAll command to exclude default item

--- bot/commands/delAll.js
+++ bot/commands/delAll.js
@@ -7,31 +7,49 @@ module.exports = {
         .setDescription('Delete all upcoming items from the database'),
     async execute(interaction) {
         try {
-            // Delete all upcoming items from the database
-            dbConnection.query('DELETE FROM upcoming', (err) => {
-                if (err) {
-                    console.error('Error:', err);
+            // Check the count of upcoming items before deletion
+            dbConnection.query('SELECT COUNT(*) AS count FROM upcoming WHERE id <> 1', (countErr, result) => {
+                if (countErr) {
+                    console.error('Error:', countErr);
                     const errorEmbed = new EmbedBuilder()
                         .setTitle('Error!')
-                        .setDescription('An error occurred while trying to delete all upcoming items from the database.')
+                        .setDescription('An error occurred while trying to delete upcoming items from the database.')
                         .setColor('#342B7E')
                         .setTimestamp();
                     interaction.reply({ embeds: [errorEmbed] });
                 }
                 else {
-                    const successEmbed = new EmbedBuilder()
-                        .setTitle('Success!')
-                        .setDescription('All upcoming items have been deleted from the database.')
-                        .setColor('#342B7E')
-                        .setTimestamp();
-                    interaction.reply({ embeds: [successEmbed] });
+                    const count = result[0].count;
 
-                    // Insert a default item into the database after deletion
-                    const defaultItem = ['No upcoming items', '0', '0'];
-                    const sql = 'INSERT INTO `upcoming` (`info`, `userId`, `userName`) VALUES (?, ?, ?)';
-                    dbConnection.query(sql, defaultItem, (insertErr) => {
-                        if (insertErr) {
-                            console.error('Error:', insertErr);
+                    // Delete all upcoming items from the database excluding item with ID 1
+                    dbConnection.query('DELETE FROM upcoming WHERE id <> 1', (deleteErr) => {
+                        if (deleteErr) {
+                            console.error('Error:', deleteErr);
+                            const deleteErrorEmbed = new EmbedBuilder()
+                                .setTitle('Error!')
+                                .setDescription('An error occurred while trying to delete upcoming items from the database.')
+                                .setColor('#342B7E')
+                                .setTimestamp();
+                            interaction.reply({ embeds: [deleteErrorEmbed] });
+                        }
+                        else {
+                            const successEmbed = new EmbedBuilder()
+                                .setTitle('Success!')
+                                .setDescription(`All upcoming items except the default item have been deleted from the database.\n **${count}** items deleted.`)
+                                .setColor('#342B7E')
+                                .setTimestamp();
+                            interaction.reply({ embeds: [successEmbed] });
+
+                            // If there are no remaining items, insert a default item into the database after deletion
+                            if (count === 0) {
+                                const defaultItem = ['No upcoming items', '0', '0'];
+                                const sql = 'INSERT INTO `upcoming` (`info`, `userId`, `userName`) VALUES (?, ?, ?)';
+                                dbConnection.query(sql, defaultItem, (insertErr) => {
+                                    if (insertErr) {
+                                        console.error('Error:', insertErr);
+                                    }
+                                });
+                            }
                         }
                     });
                 }
